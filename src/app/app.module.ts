d88f4eca533072b3ee4f6842ed07a9377c9936a4
mhmdlhsn97 <mhmd.lhsn@outlook.com>
2023-09-12 23:08:53 +0300

    create issues list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { IssueListComponent } from './issue-list/issue-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
