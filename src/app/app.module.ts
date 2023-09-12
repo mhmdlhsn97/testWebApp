5ef5157cc6d19926018e128bcfe043b3c5070297
mhmdlhsn97 <mhmd.lhsn@outlook.com>
2023-09-12 23:02:18 +0300

    install clarity library and import in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
