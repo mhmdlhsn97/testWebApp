a417dbf3b277954618b03b9e552a398d4e2a9eac
mhmdlhsn97 <mhmd.lhsn@outlook.com>
2023-09-12 23:12:06 +0300

    inject issue service

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { IssuesService } from '../issues.service';
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
   styleUrls: ['./issue-list.component.css']
 })
 export class IssueListComponent {
-
+constructor(private issueService: IssuesService){}
 }
